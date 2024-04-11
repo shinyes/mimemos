928d38dc6d0ff0a534ae15cdd749a6d866767057
cyk <cykyes@qq.com>
2024-04-11 15:04:06 +0800

    修改注释

--- front_end/destop/scripts/script_of_login/login_check_token_exists.js
+++ front_end/destop/scripts/script_of_login/login_check_token_exists.js
@@ -1,4 +1,4 @@
-// 本脚本用于检查是否已经有 token，如果有，则直接跳转至 index 页面
+// 此脚本用于检查 cookie 中是否有 token 字段，有则跳转至 index 页面
 const cookies = document.cookie.split(';')
 let token_exists = false;
 cookies.forEach(cookie => {
