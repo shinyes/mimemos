a4a7e38e057ace6cf57decfb35118b2c2d7fa839
cyk <cykyes@qq.com>
2024-04-19 13:55:27 +0800

    去除连接路径时不必要的斜杠

--- back_end/load_config.py
+++ back_end/load_config.py
@@ -7,10 +7,10 @@ with open(os.path.join(current_dir, 'config.json')) as f:
     config = json.load(f)
 
 # 部署的目录
-root_dir = os.path.join(current_dir, '../')
+root_dir = os.path.join(current_dir, '..')
 host = config['host']
-main_data_path = os.path.join(current_dir, '../', config['main_data_path'])
-database_file_path = os.path.join(current_dir, '../', config['database_file_path'])
+main_data_path = os.path.join(current_dir, '..', config['main_data_path'])
+database_file_path = os.path.join(current_dir, '..', config['database_file_path'])
 
 # 避免外部访问
 del config
