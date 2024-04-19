a3dc27fd453a0843b0c7ca90b1a3c37ec9c206a0
cyk <cykyes@qq.com>
2024-04-19 14:21:46 +0800

    删除无用的代码

--- front_end/modules/a/demo-main.js
+++
@@ -1,9 +0,0 @@
-import { create_rescs } from "./project_specific_mods/rescs/rescs.mjs"
-
-let file_input = document.querySelector('#file-input')
-
-let rescs = create_rescs(file_input)
-
-let btm = document.querySelector("#btm")
-let conatiner = document.querySelector("#container")
-conatiner.insertBefore(rescs, btm)
